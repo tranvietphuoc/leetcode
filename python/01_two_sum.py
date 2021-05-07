8ce81c3cf092ebe3f12c3da32a24864c9829e3b7
Tran Viet Phuoc <phuoc.finn@gmail.com>
2021-05-07 23:58:49 +0700

    hash_table

--- python/01_two_sum.py
+++ python/01_two_sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        temp_list = []
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    temp_list.append(i)
-                    temp_list.append(j)
-                    return temp_list
+        # temp_list = []
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             temp_list.append(i)
+        #             temp_list.append(j)
+        #             return temp_list
+
+        hash_table = {}
+        for i, num in enumerate(nums):
+            remaining = target - num
+            if remaining not in hash_table:
+                hash_table[num] = i
+            else:
+                return [hash_table[remaining], i]
+
